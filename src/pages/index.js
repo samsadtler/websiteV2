d00fd776ae1a16fa2c661269311d3b70bbd899d9
Sam Sadtler <sam.sadtler@gmail.com>
2019-12-15 10:27:34 -0500

    saving images with uneven border

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,37 +6,47 @@ import { Container, Row, Col } from 'react-bootstrap'
 import { Image } from "../components/image"
 
 export default function Index({ data }) {
-  const { edges: posts } = data.allMarkdownRemark
+    const { edges: posts } = data.allMarkdownRemark
+    var randomBorderRadius = () => {
+        const min = 20;
+        const style = {
+            borderTopLeftRadius: Math.random() * 100 + "%",
+            borderTopRightRadius: Math.random() * 100 + "%",
+            borderBottomRightRadius: Math.random() * 100 + "%",
+            borderBottomLeftRadius: Math.random() * 100 + "%",
+        };
+        return style
+    }
 
-  return (
-    <Layout>
-      <div className="blog-posts">
-        <Container fluid>
-          <Row>
-            {posts
-              .filter(post => post.node.frontmatter.title.length > 0)
-              .map(({ node: post
-              }) => {
-                let imgMainFluid = post.frontmatter.imgMain.childImageSharp.fluid
-                return (
+    return (
+        <Layout>
+            <div className="blog-posts">
+                <Container fluid>
+                    <Row>
+                        {posts
+                            .filter(post => post.node.frontmatter.title.length > 0)
+                            .map(({ node: post
+                            }) => {
+                                let imgMainFluid = post.frontmatter.imgMain.childImageSharp.fluid
+                                return (
 
-                  <Col md={4} sm={12} style={{ padding: '2px' }}>
-                    <Link to={post.frontmatter.path}>
-                      <div className="blog-post-preview" key={post.id}>
-                        <Img fluid={imgMainFluid} style={{ maxWidth: "400px" }} />
-                        <p className="hoverPostExcerpt"> {post.description}</p>
-                      </div>
-                    </Link>
-                  </Col>
+                                    <Col md={4} sm={12} style={{ padding: '2px' }}>
+                                        <Link to={post.frontmatter.path}>
+                                            <div className="blog-post-preview" key={post.id}>
+                                                <Img className="img-warp" fluid={imgMainFluid} style={{ maxWidth: "400px" }, randomBorderRadius()} />
+                                                <p className="hoverPostExcerpt"> {post.description}</p>
+                                            </div>
+                                        </Link>
+                                    </Col>
 
-                )
-              })
-            }
-          </Row>
-        </Container>
-      </div>
-    </Layout>
-  )
+                                )
+                            })
+                        }
+                    </Row>
+                </Container>
+            </div>
+        </Layout>
+    )
 }
 
 export const pageQuery = graphql`
